d3aa7a6632894710a1eec9dfc7f2cac01a9cde13
lijie <wynn@MacBook-Pro.local>
2021-02-21 03:53:54 +0800

    ws_host

--- server.py
+++ server.py
@@ -60,13 +60,13 @@ class webSSHServer(tornado.websocket.WebSocketHandler):
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
-        if(self.WsHost == ''):
+        if(WsHost == ''):
             s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             s.connect(('8.8.8.8', 80))
             print(s.getsockname()[0])
-            self.WsHost = s.getsockname()[0]
+            WsHost = s.getsockname()[0]
             s.close()
-        self.render("index.html",ws_host=self.WsHost)
+        self.render("index.html",ws_host=WsHost)
 
 if __name__ == '__main__':
     # 设置静态文件
